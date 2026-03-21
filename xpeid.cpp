409f4246fdc51fe57179e6e81f9b4293f995389f
horsicq <horsicq@gmail.com>
2026-03-21 11:07:54 +0100

    feat: add QtConcurrent support, refactor signature loading

--- xpeid.cpp
+++ xpeid.cpp
@@ -24,6 +24,8 @@
 #include "xbinary.h"
 #include <QFile>
 #include <QTextStream>
+#include <QElapsedTimer>
+#include <QtConcurrent/QtConcurrent>
 
 XPEID::XPEID(QObject *pParent) : XScanEngine(pParent)
 {
@@ -46,92 +48,6 @@ static QString _normalizeSignature(const QString &sSignature)
     return XBinary::convertSignature(sSignature);
 }
 
-// bool XPEID::loadDatabase(const QString &sDatabasePath, XBinary::PDSTRUCT *pPdStruct)
-// {
-//     m_listSignatures.clear();
-
-//     QString sPath = sDatabasePath;
-
-//     QFileInfo fi(sPath);
-//     if (fi.isDir()) {
-//         QString sCandidate = QDir(sPath).filePath("userdb.txt");
-//         if (QFileInfo::exists(sCandidate)) {
-//             sPath = sCandidate;
-//         }
-//     }
-
-//     if (!QFileInfo::exists(sPath)) {
-//         return false;
-//     }
-
-//     QFile file(sPath);
-//     if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-//         return false;
-//     }
-
-//     QTextStream stream(&file);
-//     QString sLine;
-//     QString sSection;
-
-//     while (!stream.atEnd()) {
-//         sLine = stream.readLine();
-//         if (sLine.trimmed().isEmpty()) {
-//             continue;
-//         }
-
-//         if (_isUserDBComment(sLine)) {
-//             continue;
-//         }
-
-//         if (sLine.startsWith("[")) {
-//             // Section header: [something]
-//             int nPos = sLine.indexOf("]");
-//             if (nPos != -1) {
-//                 sSection = sLine.mid(1, nPos - 1).trimmed();
-//             }
-//             continue;
-//         }
-
-//         // Typical PEiD signature line: <name> <hex pattern> [; comment]
-//         QString sClean = sLine;
-//         int nCommentPos = sClean.indexOf(";");
-//         if (nCommentPos >= 0) {
-//             sClean = sClean.left(nCommentPos);
-//         }
-//         sClean = sClean.trimmed();
-//         if (sClean.isEmpty()) {
-//             continue;
-//         }
-
-//         // Split first token as name, rest is signature pattern
-//         QStringList parts = sClean.split(QRegExp("\\s+"), Qt::SkipEmptyParts);
-//         if (parts.count() < 2) {
-//             continue;
-//         }
-
-//         QString sName = parts.takeFirst();
-//         QString sType = sSection;
-
-//         QString sSignature = parts.join(" ");
-//         sSignature = _normalizeSignature(sSignature);
-
-//         if (sSignature.isEmpty()) {
-//             continue;
-//         }
-
-//         SIGNATURE_RECORD record = {};
-//         record.sName = sName;
-//         record.sSignature = sSignature;
-//         record.sType = sType;
-//         record.sDescription = QString();
-
-//         m_listSignatures.append(record);
-//     }
-
-//     file.close();
-
-//     return (m_listSignatures.count() > 0);
-// }
 
 static QString _getTypeFromFileName(const QString &sFilePath)
 {
@@ -174,8 +90,11 @@ QList<XScanEngine::SIGNATURE_RECORD> XPEID::getSignaturesFromData(const QString 
     qint32 nNumberOfLines = listLines.count();
 
     QString sCurrentName;
+    QString sCurrentVersion;
+    QString sCurrentInfo;
     QString sCurrentSignature;
     bool bCurrentEpOnly = true;
+    qint64 nCurrentLine = 0;
 
     for (qint32 i = 0; (i < nNumberOfLines) && XBinary::isPdStructNotCanceled(pPdStruct); i++) {
         QString sLine = listLines.at(i).trimmed();
@@ -196,18 +115,61 @@ QList<XScanEngine::SIGNATURE_RECORD> XPEID::getSignaturesFromData(const QString 
                 record.sFilePath = sSignatureFilePath;
                 record.sType = sType;
                 record.sName = sCurrentName;
+                record.sVersion = sCurrentVersion;
+                record.sInfo = sCurrentInfo;
                 record.sText = _normalizeSignature(sCurrentSignature);
-                record.sInfo = bCurrentEpOnly ? QString("ep_only") : QString();
+                record.bIsEP = bCurrentEpOnly;
+                record.nLine = nCurrentLine;
 
                 listResult.append(record);
             }
 
             // Start new entry
+            nCurrentLine = i + 1;
             qint32 nPos = sLine.indexOf("]");
             if (nPos != -1) {
                 sCurrentName = sLine.mid(1, nPos - 1).trimmed();
             } else {
                 sCurrentName = sLine.mid(1).trimmed();
+            }
+            // [!EP (ExE Pack) V1.0 -> Elite Coding Group]
+            sCurrentVersion.clear();
+            sCurrentInfo.clear();
+            qint32 nArrowPos = sCurrentName.indexOf("->");
+            if (nArrowPos != -1) {
+                sCurrentInfo = sCurrentName.mid(nArrowPos + 2).trimmed();
+                sCurrentName = sCurrentName.left(nArrowPos).trimmed();
+            }
+            // Extract version: first token starting with V/v+digit or a bare digit
+            {
+                qint32 nVersionPos = -1;
+                qint32 nNameLen = sCurrentName.length();
+                for (qint32 j = 0; j < nNameLen; ) {
+                    while (j < nNameLen && sCurrentName.at(j) == QChar(' ')) {
+                        j++;
+                    }
+                    if (j >= nNameLen) {
+                        break;
+                    }
+                    QChar cFirst = sCurrentName.at(j);
+                    bool bIsVersion = false;
+                    if ((cFirst == QChar('v') || cFirst == QChar('V')) && (j + 1 < nNameLen) && sCurrentName.at(j + 1).isDigit()) {
+                        bIsVersion = true;
+                    } else if (cFirst.isDigit()) {
+                        bIsVersion = true;
+                    }
+                    if (bIsVersion && j > 0) {
+                        nVersionPos = j;
+                        break;
+                    }
+                    while (j < nNameLen && sCurrentName.at(j) != QChar(' ')) {
+                        j++;
+                    }
+                }
+                if (nVersionPos != -1) {
+                    sCurrentVersion = sCurrentName.mid(nVersionPos).trimmed();
+                    sCurrentName = sCurrentName.left(nVersionPos).trimmed();
+                }
             }
 
             sCurrentSignature.clear();
@@ -244,14 +206,88 @@ QList<XScanEngine::SIGNATURE_RECORD> XPEID::getSignaturesFromData(const QString 
         record.sFilePath = sSignatureFilePath;
         record.sType = sType;
         record.sName = sCurrentName;
+        record.sVersion = sCurrentVersion;
+        record.sInfo = sCurrentInfo;
         record.sText = _normalizeSignature(sCurrentSignature);
-        record.sInfo = bCurrentEpOnly ? QString("ep_only") : QString();
+        record.bIsEP = bCurrentEpOnly;
+        record.nLine = nCurrentLine;
 
         listResult.append(record);
     }
 
     return listResult;
 }
+
+struct _XPEID_SCAN_CONTEXT {
+    const QList<XScanEngine::SIGNATURE_RECORD> *pListSignatures;
+    const QByteArray *pbaData;
+    qint64 nSize;
+    XScanEngine::SCANID resultId;
+    XScanEngine::SCANID parentId;
+    QList<XScanEngine::SCANSTRUCT> *pListMatches;
+    QList<XScanEngine::DEBUG_RECORD> *pListDebugRecords;
+    QMutex *pMutex;
+    XBinary::PDSTRUCT *pPdStruct;
+    qint32 nFreeIndex;
+    bool bShowScanTime;
+
+    void operator()(const qint32 &nIndex)
+    {
+        if (!XBinary::isPdStructNotCanceled(pPdStruct)) {
+            return;
+        }
+
+        const XScanEngine::SIGNATURE_RECORD &signature = pListSignatures->at(nIndex);
+
+        QElapsedTimer elapsedTimer;
+        elapsedTimer.start();
+
+        QBuffer buffer;
+        buffer.setData(*pbaData);
+        buffer.open(QIODevice::ReadOnly);
+
+        XBinary bin(&buffer);
+        XBinary::_MEMORY_MAP mm = bin.getMemoryMap(XBinary::MAPMODE_UNKNOWN, pPdStruct);
+
+        bool bMatch = bin.isSignaturePresent(&mm, 0, nSize, signature.sText, pPdStruct);
+
+        qint64 nElapsedTime = elapsedTimer.elapsed();
+
+        if (bMatch) {
+            XScanEngine::SCANSTRUCT ss = {};
+            ss.id = resultId;
+            ss.parentId = parentId;
+            ss.sType = signature.sType;
+            ss.sName = signature.sName;
+            ss.type = XScanEngine::recordTypeStringToId(signature.sType);
+            ss.name = XScanEngine::recordNameStringToId(signature.sName);
+            ss.sVersion = signature.sVersion;
+            ss.sInfo = signature.sInfo;
+            ss.bIsUnknown = false;
+            ss.bIsHeuristic = false;
+            ss.bIsAHeuristic = false;
+
+            QMutexLocker locker(pMutex);
+            pListMatches->append(ss);
+        }
+
+        {
+            QMutexLocker locker(pMutex);
+
+            if (bShowScanTime) {
+                XScanEngine::DEBUG_RECORD debugRecord = {};
+                debugRecord.sScript = signature.sName;
+                debugRecord.sType = signature.sType;
+                debugRecord.sName = signature.sName;
+                debugRecord.nElapsedTime = nElapsedTime;
+                debugRecord.nLine = signature.nLine;
+                pListDebugRecords->append(debugRecord);
+            }
+
+            XBinary::setPdStructCurrentIncrement(pPdStruct, nFreeIndex);
+        }
+    }
+};
 
 void XPEID::_processDetect(XScanEngine::SCANID *pScanID, XScanEngine::SCAN_RESULT *pScanResult, QIODevice *pDevice, const SCANID &parentId,
                             XBinary::FT fileType, XScanEngine::SCAN_OPTIONS *pOptions, bool bAddUnknown, XBinary::PDSTRUCT *pPdStruct)
@@ -271,34 +307,117 @@ void XPEID::_processDetect(XScanEngine::SCANID *pScanID, XScanEngine::SCAN_RESUL
     XBinary::_MEMORY_MAP memoryMap = binary.getMemoryMap(XBinary::MAPMODE_UNKNOWN, pPdStruct);
     qint64 nSize = memoryMap.nBinarySize;
 
+    qint32 nNumberOfSignatures = m_listSignatures.count();
+
+    // Separate EP and non-EP signature indices
+    QList<qint32> listEpIndices;
+    QList<qint32> listNonEpIndices;
+    qint32 nMaxEpSigBytes = 0;
+
+    for (qint32 i = 0; i < nNumberOfSignatures; i++) {
+        const SIGNATURE_RECORD &sig = m_listSignatures.at(i);
+
+        if (sig.bIsEP) {
+            listEpIndices.append(i);
+            qint32 nSigBytes = sig.sText.length() / 2;
+
+            if (nSigBytes > nMaxEpSigBytes) {
+                nMaxEpSigBytes = nSigBytes;
+            }
+        } else {
+            listNonEpIndices.append(i);
+        }
+    }
+
+    // Get entry point offset and read EP signature once
+    qint64 nEpOffset = XFormats::getEntryPointOffset(fileType, pDevice);
+    QString sEpSignature;
+
+    if ((nMaxEpSigBytes > 0) && (nEpOffset > 0) && (nEpOffset < nSize)) {
+        sEpSignature = binary.getSignature(nEpOffset, nMaxEpSigBytes).toLower();
+    }
+
     XScanEngine::SCANID resultId = {};
     resultId.sUuid = XBinary::generateUUID();
     resultId.fileType = fileType;
     resultId.filePart = XBinary::FILEPART_HEADER;
     resultId.nSize = nSize;
 
-    for (qint32 i = 0; i < m_listSignatures.count(); i++) {
-        if (XBinary::isPdStructNotCanceled(pPdStruct)) {
-            const SIGNATURE_RECORD &signature = m_listSignatures.at(i);
+    qint32 _nFreeIndex = XBinary::getFreeIndex(pPdStruct);
+    XBinary::setPdStructInit(pPdStruct, _nFreeIndex, nNumberOfSignatures);
 
-            if (binary.isSignaturePresent(&memoryMap, 0, nSize, signature.sText, pPdStruct)) {
-                XScanEngine::SCANSTRUCT ss = {};
-                ss.id = resultId;
-                ss.parentId = parentId;
-                ss.sType = signature.sType;
-                ss.sName = signature.sName;
-                ss.type = XScanEngine::recordTypeStringToId(signature.sType);
-                ss.name = XScanEngine::recordNameStringToId(signature.sName);
-                ss.sVersion = signature.sVersion;;
-                ss.sInfo = signature.sInfo;
-                ss.bIsUnknown = false;
-                ss.bIsHeuristic = false;
-                ss.bIsAHeuristic = false;
+    // Phase 1: EP signatures (fast string compare, sequential)
+    for (qint32 i = 0; (i < listEpIndices.count()) && XBinary::isPdStructNotCanceled(pPdStruct); i++) {
+        const SIGNATURE_RECORD &signature = m_listSignatures.at(listEpIndices.at(i));
 
-                pScanResult->listRecords.append(ss);
-            }
+        QElapsedTimer elapsedTimer;
+        elapsedTimer.start();
+
+        bool bMatch = false;
+
+        if (!sEpSignature.isEmpty()) {
+            bMatch = XBinary::compareSignatureStrings(sEpSignature, signature.sText);
         }
+
+        qint64 nElapsedTime = elapsedTimer.elapsed();
+
+        if (bMatch) {
+            XScanEngine::SCANSTRUCT ss = {};
+            ss.id = resultId;
+            ss.parentId = parentId;
+            ss.sType = signature.sType;
+            ss.sName = signature.sName;
+            ss.type = XScanEngine::recordTypeStringToId(signature.sType);
+            ss.name = XScanEngine::recordNameStringToId(signature.sName);
+            ss.sVersion = signature.sVersion;
+            ss.sInfo = signature.sInfo;
+            ss.bIsUnknown = false;
+            ss.bIsHeuristic = false;
+            ss.bIsAHeuristic = false;
+
+            pScanResult->listRecords.append(ss);
+        }
+
+        if (pOptions->bShowScanTime) {
+            XScanEngine::DEBUG_RECORD debugRecord = {};
+            debugRecord.sScript = signature.sName;
+            debugRecord.sType = signature.sType;
+            debugRecord.sName = signature.sName;
+            debugRecord.nElapsedTime = nElapsedTime;
+            debugRecord.nLine = signature.nLine;
+            pScanResult->listDebugRecords.append(debugRecord);
+        }
+
+        XBinary::setPdStructCurrentIncrement(pPdStruct, _nFreeIndex);
     }
+
+    // Phase 2: Non-EP signatures in parallel (scan through file data)
+    if (!listNonEpIndices.isEmpty() && XBinary::isPdStructNotCanceled(pPdStruct)) {
+        QByteArray baData = binary.read_array(0, nSize);
+        QMutex mutex;
+        QList<XScanEngine::SCANSTRUCT> listMatches;
+        QList<XScanEngine::DEBUG_RECORD> listDebugRecords;
+
+        _XPEID_SCAN_CONTEXT context = {};
+        context.pListSignatures = &m_listSignatures;
+        context.pbaData = &baData;
+        context.nSize = nSize;
+        context.resultId = resultId;
+        context.parentId = parentId;
+        context.pListMatches = &listMatches;
+        context.pListDebugRecords = &listDebugRecords;
+        context.pMutex = &mutex;
+        context.pPdStruct = pPdStruct;
+        context.nFreeIndex = _nFreeIndex;
+        context.bShowScanTime = pOptions->bShowScanTime;
+
+        QtConcurrent::blockingMap(listNonEpIndices, context);
+
+        pScanResult->listRecords.append(listMatches);
+        pScanResult->listDebugRecords.append(listDebugRecords);
+    }
+
+    XBinary::setPdStructFinished(pPdStruct, _nFreeIndex);
 
     if (pScanID) {
         *pScanID = resultId;
