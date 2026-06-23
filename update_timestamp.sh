15c2e2951ab2443c7794e8f88c9fc5c65b217f28
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2026-06-23 13:52:46 +0200

    fix typo in update_timestamp.sh

--- update_timestamp.sh
+++ update_timestamp.sh
@@ -2,4 +2,4 @@
 echo "date=$(date +'%Y-%m-%d')" > peid/info.ini
 git add peid/info.ini
 git commit -m "Updated timestamp for info.ini" >/dev/null 2>&1 && git push
-echo "Datex updated in peid/info.ini to the current date"
+echo "Date updated in peid/info.ini to the current date"
